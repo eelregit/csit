ac7148da5a73ef43feaf0cb54b2d644b934946d7
Yin Li <eelregit@gmail.com>
2020-02-07 09:36:42 -0800

    change params and Makefile for popeye

---
+++ ic/script/2lpt_iso.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --partition=general
+#SBATCH --nodes=6
+#SBATCH --ntasks=288
+#SBATCH --time=4-00:00:00
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --output=stdout_iso
+#SBATCH --workdir=/home/yinli/csit/ic/
+#SBATCH --mail-user=kazuyuki.akitsu@ipmu.jp
+#SBATCH --mail-type=END
+#SBATCH --job-name=ic
+
+/usr/mpi/gcc/openmpi-2.1.2-hfi/bin/mpirun -np 288 ./2LPTic ./masaki_params/masaki_iso.param
---
+++ lgadget2/script/lgadget2_ppm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --partition=general
+#SBATCH --nodes=5
+#SBATCH --ntasks=240
+#SBATCH --time=4-00:00:00
+#SBATCH --mem-per-cpu=8GB
+#SBATCH --output=stdout_iso
+#SBATCH --workdir=/home/yinli/csit/lgadget2/
+#SBATCH --mail-user=kazuyuki.akitsu@ipmu.jp
+#SBATCH --mail-type=END
+#SBATCH --job-name=nb
+
+/usr/mpi/gcc/openmpi-2.1.2-hfi/bin/mpirun -np 240 ./L-Gadget2 ./masaki_params/params_ppm.txt
