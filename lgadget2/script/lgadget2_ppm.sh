f596139e8f0895b8a206d23a6075bc633956dee5
kautumntsu <kzyk.akt.7@gmail.com>
2020-03-17 23:50:13 +0900

    Clean up lgadget2 folder

--- lgadget2/script/lgadget2_ppm.sh
+++ lgadget2/script/lgadget2_ppm.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+#SBATCH --job-name=nb
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=kazuyuki.akitsu@ipmu.jp
+
 #SBATCH --partition=general
+
+#SBATCH --exclusive
 #SBATCH --nodes=5
 #SBATCH --ntasks=240
-#SBATCH --time=4-00:00:00
-#SBATCH --mem-per-cpu=8GB
-#SBATCH --output=stdout_iso
-#SBATCH --workdir=/home/yinli/csit/lgadget2/
-#SBATCH --mail-user=kazuyuki.akitsu@ipmu.jp
-#SBATCH --mail-type=END
-#SBATCH --job-name=nb
+#SBATCH --time=2-00:00:00
 
-/usr/mpi/gcc/openmpi-2.1.2-hfi/bin/mpirun -np 240 ./L-Gadget2 ./masaki_params/params_ppm.txt
+#SBATCH --chdir=/home/yinli/csit/lgadget2
+
+module load gcc openmpi lib/fftw2/2.1.5-openmpi1 lib/gsl
+
+hostname; pwd; date
+
+srun --mpi=pmi2 ./L-Gadget2 ./planck2015_params/params_ppm.txt
+
+date
