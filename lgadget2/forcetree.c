a943e75c4a9b959b6773105fb68ce3141f78fd96
kautumntsu <kzyk.akt.7@gmail.com>
2020-10-03 04:27:32 +0900

    Fix a stupid bug in forcetree.c

--- lgadget2/forcetree.c
+++ lgadget2/forcetree.c
@@ -845,16 +845,18 @@ int force_treeevaluate_shortrange(int target, int mode, FLOAT * acc)
 	} else 
 	{
 	  u = r * h_inv;
-	  if(u < 0.5)
+	  if(u < 0.5){
 	    fac1 = mass * h3_inv * (10.666666666667 + u * u * (32.0 * u - 38.4));
 		fac1 *= Jacobi * mass * r2 * r / (2.*sqrt(M_PI));
 		fac1 /= All.Asmth[0];
-	  else
-	    fac1 =
+	}
+	 else{
+	  	fac1 =
 	      mass * h3_inv * (21.333333333333 - 48.0 * u +
 			       38.4 * u * u - 10.666666666667 * u * u * u - 0.066666666667 / (u * u * u));
 		fac1 *= Jacobi * mass * r2 * r / (2.*sqrt(M_PI));
 		fac1 /= All.Asmth[0];
+		}
 	}
 
       if(tabindex < NTAB-1)
